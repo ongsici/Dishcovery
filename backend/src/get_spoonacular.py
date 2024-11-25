b5c89835382ac76acc46bb166a97efcfc8d3dddb
ongsici <ongsici@gmail.com>
2024-11-25 16:35:14 +0000

    feat: added get recipe by id and get nutrition by id from spoonacular

--- backend/src/get_spoonacular.py
+++ backend/src/get_spoonacular.py
@@ -6,12 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+
 def get_recipe_by_ingredients(ingredients_list: List):
     url = "https://api.spoonacular.com/recipes/findByIngredients"
     ingredients = ", ".join(ingredients_list)
     params ={
         "ingredients": ingredients,
-        "apiKey": API_KEY
+        "apiKey": API_KEY,
+        "number": 10
     }
     response = requests.get(url, params=params)
     data = response.json()
@@ -19,5 +21,21 @@ def get_recipe_by_ingredients(ingredients_list: List):
     return data
 
 
-get_recipe_by_ingredients(["apples, sugar, flour"])
+def get_recipe_by_id(recipeID: int):
+    url = f'https://api.spoonacular.com/recipes/{recipeID}/information'
+    params = {
+        "apiKey": API_KEY
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
 
+
+def get_nutrition_by_id(recipeID: int):
+    url = f'https://api.spoonacular.com/recipes/{recipeID}/nutritionWidget.json'
+    params = {
+        "apiKey": API_KEY
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
