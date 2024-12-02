8e1cc2771dbd92354207c7587242f084d12c9ca1
Ashley Chung <ashjchung@gmail.com>
2024-12-02 12:28:39 +0000

    feat: SavedRecipe database model

--- src/app.py
+++ src/app.py
@@ -3,13 +3,28 @@ from flask import Flask, render_template, request
 from src.utils.data_models import IngredientsInput
 from src.utils.get_spoonacular import get_nutrition_by_id, get_recipe_by_id, get_recipe_by_ingredients
 from src.utils.get_nutrition_intake import get_daily_nutrition_intake
+from src.database import database as db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:/username:password@db//saved_recipes.db" # TODO
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app) # Set up extensions
+db.create_all()
+db.drop_all()
 
 results = {}
 
-# from flask import redirect, url_for
-# from flask_sqlalchemy import SQLAlchemy
+# Define the database model
+class SavedRecipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    image = db.Column(db.String(200), nullable=False)
+    instructions = db.Column(db.Text, nullable=True)
+    calories = db.Column(db.String(50), nullable=True)
+    carbohydrate = db.Column(db.String(50), nullable=True)
+    fat = db.Column(db.String(50), nullable=True)
+    protein = db.Column(db.String(50), nullable=True)
 
 
 @app.route("/")
@@ -110,7 +125,7 @@ def recipe_search_results():
     return render_template("recipe_search_results.html", recipes=results)
 
 
-# Route for the recipe details page
+# @app.route("/recipe_details.html")
 @app.route('/recipe/<int:recipe_id>')
 def recipe_details(recipe_id):
     recipe = results.get(recipe_id)
@@ -119,7 +134,7 @@ def recipe_details(recipe_id):
     return render_template('recipe_details.html', recipe=recipe)
 
 
-# for debugging
+# For debugging
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
@@ -147,47 +162,31 @@ def nutrition_query():
 
 
 @app.route("/saved_recipes")
-def saved_recipes():
+def saved_recipes(recipe_id):
+    # database.session.add(results.get(recipe_id))
+    # database.session.commit()
     # saved_recipes = SavedRecipe.query.all()
     return render_template("saved_recipes.html")
     # , recipes=saved_recipes
 
 
-# TODO: Initialize the database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'  # Example: SQLite database
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+# Write saved recipe to database
+@app.route('/save_recipe', methods=['POST'])
+def save_recipe():
+    recipe_id = request.form.get('recipe_id')
+    recipe = results.get(int(recipe_id))
 
-# TODO: Define the database model
-# class SavedRecipe(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(150), nullable=False)
-#     image = db.Column(db.String(200), nullable=False)
-#     instructions = db.Column(db.Text, nullable=True)
-#     calories = db.Column(db.String(50), nullable=True)
-#     carbohydrate = db.Column(db.String(50), nullable=True)
-#     fat = db.Column(db.String(50), nullable=True)
-#     protein = db.Column(db.String(50), nullable=True)
-
-# TODO: Write saved recipes to db
-# @app.route('/save_recipe', methods=['POST'])
-# def save_recipe():
-#     recipe_id = request.form.get('recipe_id')
-#     recipe = results.get(int(recipe_id))
-
-#     if recipe:
-#         saved_recipe = SavedRecipe(
-#             id=recipe['id'],
-#             name=recipe['name'],
-#             image=recipe['image'],
-#             instructions=recipe['instructions'],
-#             calories=recipe['nutrition']['calories'],
-#             carbohydrate=recipe['nutrition']['carbohydrate'],
-#             fat=recipe['nutrition']['fat'],
-#             protein=recipe['nutrition']['protein'],
-#         )
-#         db.session.add(saved_recipe)
-#         db.session.commit()
-#         return redirect(url_for('saved_recipes'))
-#     else:
-#         return "Recipe not found", 404
+    if recipe:
+        saved_recipe = SavedRecipe(
+            id=recipe['id'],
+            name=recipe['name'],
+            image=recipe['image'],
+            instructions=recipe['instructions'],
+            calories=recipe['nutrition']['calories'],
+            carbohydrate=recipe['nutrition']['carbohydrate'],
+            fat=recipe['nutrition']['fat'],
+            protein=recipe['nutrition']['protein'],
+        )
+        db.session.add(saved_recipe)
+        db.session.commit()
+        # return redirect(url_for('saved_recipes')) TODO: Fix
